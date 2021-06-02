184853f154fd1d8cf07c76fd85eef495b13dd39b
dunstad <anthony00.blount00@gmail.com>
2021-06-03 08:56:28 +0900

    functions on checkboxes

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SetExplosions(bool explosions)
     {
-        
+        Debug.Log("explosions" + explosions);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetRelaxedMode(bool relaxedMode)
     {
-        
+        Debug.Log("relaxed" + relaxedMode);
     }
 }
