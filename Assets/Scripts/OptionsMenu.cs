5faf9869ae7745e7ec260031470404bd5e574c13
dunstad <anthony00.blount00@gmail.com>
2021-06-03 21:29:28 +0900

    options loaded in menu too, so they display properly

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public GameObject explosionsCheck;
+    public GameObject relaxedModeCheck;
+    void Start()
+    {
+        bool explosions = PlayerPrefs.GetInt("explosions", 1) == 0 ? false : true;
+        explosionsCheck.GetComponent<Toggle>().isOn = explosions;
+        bool relaxedMode = PlayerPrefs.GetInt("relaxedMode", 0) == 0 ? false : true;
+        relaxedModeCheck.GetComponent<Toggle>().isOn = relaxedMode;
+    }
+
     public void SetExplosions(bool explosions)
     {
         int explosionInt = explosions ? 1 : 0;
